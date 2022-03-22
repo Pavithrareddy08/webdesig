36d7af8ad101902ed117449ad02f44c571ab2408
Pavithrareddy08 <pavithrabasasni@gmail.com>
2022-03-21 22:22:30 -0400

    insert the data

--- Assignment_01/Script.js
+++ Assignment_01/Script.js
@@ -1,5 +1,15 @@
+var seledctedRow = null;
 function onFormSubmit(){
+    event.preventDefault();
     var formData = readFormData();
+    // insertNewRecord(formData);
+    resetForm();
+    if(seledctedRow === null){
+        insertNewRecord(formData);
+    }
+    else{
+        
+    }
 }
 
 function readFormData() {
@@ -12,4 +22,25 @@ function readFormData() {
     
 }
 
-function 
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHtml = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHtml = data.idNumber;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHtml = data.email;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHtml = data.salary;
+    // cell5 = newRow.insertCell(4);
+    // cell5.innerHtml = data.salary;
+    
+}
+
+function resetForm(){
+    document.getElementById('fullName').value = '';
+    document.getElementById('idNumber').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('salary').value = '';
+}
